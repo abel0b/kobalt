6a3ab084fedea15c742585ff4716f811ae7466ba
Abel Calluaud <abel0b@protonmail.com>
2020-11-18 11:54:32 +0100

    fix(compiler): correct bad pointer arithmetic expression

--- lib/collection/queue.c
+++ lib/collection/queue.c
@@ -28,15 +28,15 @@ void kbqueue_enqueue(struct kbqueue* queue, void* elem) {
         queue->capacity *= 2;
         queue->data = kbrealloc(queue->data, queue->capacity * queue->elem_size);
         if (queue->front + queue->size > prev_cap) {
-            memcpy(&queue->data[(prev_cap - 1) * queue->elem_size], &queue->data[0], queue->elem_size);
+            memcpy((char*)queue->data + (prev_cap - 1) * queue->elem_size, queue->data, queue->elem_size);
         }
     }
     ++ queue->size;
-    memcpy(&queue->data[queue->elem_size * ((queue->front + queue->size) % queue->capacity)], elem, queue->elem_size);
+    memcpy((char*)queue->data + queue->elem_size * ((queue->front + queue->size) % queue->capacity), elem, queue->elem_size);
 }
 
 void kbqueue_dequeue(struct kbqueue* queue, void* elem) {
     queue->size --;
-    memcpy(elem, &queue->data[queue->elem_size * queue->front], queue->elem_size);
+    memcpy(elem, (char*)queue->data + queue->elem_size * queue->front, queue->elem_size);
     queue->front = (queue->front + 1) % queue->capacity;
 }
